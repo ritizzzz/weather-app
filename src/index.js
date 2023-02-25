aef1cb9af2a024539e5b5c85c182f36263c70503
ritizzzz <kandelritiz@gmail.com>
2023-02-25 23:00:55 +1100

    mechanics of the nav bar implemented, function yet to be separated from the main js file

--- src/index.js
+++ src/index.js
@@ -21,3 +21,22 @@ const eventEmit = (() => {
 
   return { subscribe, trigger };
 })();
+
+function toggleNav() {
+  const nav = document.querySelector("nav");
+  const toggleNavButton = document.querySelector(".toggleNav");
+  if (nav.classList.contains("close")) {
+    nav.style.right = "-100%";
+    toggleNavButton.style.transform = "rotate(0deg)";
+  } else {
+    nav.style.right = "0%";
+    toggleNavButton.style.transform = "rotate(45deg)";
+  }
+  nav.classList.toggle("close");
+}
+
+eventEmit.subscribe("toggleNav", toggleNav);
+
+document.querySelector(".toggleNav").addEventListener("click", () => {
+  eventEmit.trigger("toggleNav");
+});
