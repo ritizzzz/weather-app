e44ef8fa1afb266226fa0a202b049cc621afb142
s3947204 <s3947204@student.rmit.edu.au>
2023-11-20 22:58:39 +1100

    noooo

--- src/runtimeListenersManager.js
+++ src/runtimeListenersManager.js
@@ -2,7 +2,7 @@ function listenerToNavRow(allWeather) {
   for (let i = 0; i < allWeather.length; i += 1) {
     console.log(allWeather);
     document.getElementById(allWeather[i].id).addEventListener("click", () => {
-      console.log("hello eek");
+      console.log("hello lol");
     });
   }
 }
